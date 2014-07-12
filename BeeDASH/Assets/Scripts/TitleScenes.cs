c861c91e1a9a8ebdb7671fa319afa7df955e4763
丹澤智也 <TTA2161@gmail.com>
2014-07-12 15:29:19 +0900

    タップ未完成状態

---
+++ BeeDASH/Assets/Scripts/TitleScenes.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScenes : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.touchCount == 1) 
+		//{
+			if (Input.GetMouseButtonDown(0)) {
+				Vector2 tapPoint  = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				Collider2D collition2d  = Physics2D.OverlapPoint(tapPoint);
+				if (collition2d) {
+				RaycastHit2D hitObject = Physics2D.Raycast(tapPoint,-Vector2.up);
+					if (hitObject) {
+						Debug.Log("hit object is " + hitObject.collider.gameObject.name);
+						Application.LoadLevel ("Game");
+					}
+				}
+			}
+			
+		//}
+	}
+}
