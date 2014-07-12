c71744604cadd578ea0769f701f17db7c8c89017
丹澤智也 <TTA2161@gmail.com>
2014-07-12 16:51:44 +0900

    タイトル フェードイン完成

--- BeeDASH/Assets/Scripts/TitleScenes.cs
+++ BeeDASH/Assets/Scripts/TitleScenes.cs
@@ -3,28 +3,13 @@ using System.Collections;
 
 public class TitleScenes : MonoBehaviour {
 
-
-	// Use this for initialization
 	void Start () {
 	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.touchCount == 1) 
-		{
-//			if (Input.GetMouseButtonDown(0)) {
-//				Vector2 tapPoint  = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-//				Collider2D collition2d  = Physics2D.OverlapPoint(tapPoint);
-//				if (collition2d) {
-//				RaycastHit2D hitObject = Physics2D.Raycast(tapPoint,-Vector2.up);
-//					if (hitObject) {
-//						Debug.Log("hit object is " + hitObject.collider.gameObject.name);
-						Application.LoadLevel ("Game");
-//					}
-//				}
-//			}
-			
+
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)||Input.touchCount == 1 || Input.GetMouseButtonDown(0)) {
+			FadeManager.Instance.LoadLevel("Game",0.5f);;		
 		}
+
 	}
 }
