b8e36749274d13029cdd53906fe5a198270540ab
Yama <nutahuate@gmail.com>
2014-07-12 17:56:21 +0900

    UI用スクリプト

---
+++ BeeDASH/Assets/Scripts/UI_Heart.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Heart : MonoBehaviour {
+    public static int m_hp;
+    public int m_UICount;
+
+	// Use this for initialization
+	void Start () {
+        //m_pPlayer = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(m_hp < m_UICount)
+        {
+            renderer.enabled = false;
+        }
+        else
+        {
+            renderer.enabled = true;
+        }
+	}
+}
