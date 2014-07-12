62624e158ac90f66954410a0e50095909687e657
丹澤智也 <TTA2161@gmail.com>
2014-07-12 15:51:29 +0900

    タイトル完成？

--- BeeDASH/Assets/Scripts/TitleScenes.cs
+++ BeeDASH/Assets/Scripts/TitleScenes.cs
@@ -11,44 +11,20 @@ public class TitleScenes : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		//if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.touchCount == 1) 
-		//{
-
-//		Vector2 tapPoint  = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-//		Collider2D collition2d  = Physics2D.OverlapPoint(tapPoint);
-//		RaycastHit2D hitObject = Physics2D.Raycast(tapPoint,-Vector2.up);
-//
+		if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.touchCount == 1) 
+		{
 //			if (Input.GetMouseButtonDown(0)) {
-//				if (collition2d) 
-//				{
-//					if (hitObject) 
-//					{
-						//Application.LoadLevel ("Game");
-			//		}		
-			//	}
-			//}
-			
-		//}
-
-		Ray ray ;//光線
-		RaycastHit hit ;//光線が当たったときの情報が入ります
-		Camera myCamera;//カメラを指定します。
-			
-			if(Input.touchCount == 1) {//タップあり時にtrue
-				　　　　　　 Touch touch = Input.touches[0];
-				　　　      　ray = myCamera.ScreenPointToRay(Input.touches[0].position);
-				
-				　　　　　　 if(touch.phase == TouchPhase.Ended && Physics.Raycast(ray.origin,ray.direction,out hit,Mathf.Infinity)){
-					
-					
-					if(hit.collider==gameObject.collider){
-						
+//				Vector2 tapPoint  = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//				Collider2D collition2d  = Physics2D.OverlapPoint(tapPoint);
+//				if (collition2d) {
+//				RaycastHit2D hitObject = Physics2D.Raycast(tapPoint,-Vector2.up);
+//					if (hitObject) {
+//						Debug.Log("hit object is " + hitObject.collider.gameObject.name);
 						Application.LoadLevel ("Game");
-						
-						
-					　　　}
-					
-				　 }
-			}
+//					}
+//				}
+//			}
+			
+		}
 	}
 }
